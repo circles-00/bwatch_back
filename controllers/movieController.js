16456f5b21563225e03f28717a2a2b6c27e7fc45
darknet <nkrezeski1@gmail.com>
2021-08-19 11:09:23 +0200

    Update error handling

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,71 +3,111 @@ const CatchAsync = require('../utils/catchAsync')
 
 exports.getMovie = CatchAsync(async (req, res, next) => {
   const data = await Movie.singleMovie(req.params.id)
-
-  res.status(200).json({
-    status: 'success',
-    data: data,
-  })
+  if (data) {
+    res.status(200).json({
+      status: 'success',
+      data: data,
+    })
+  } else
+    res.status(404).json({
+      status: 'failed',
+    })
 })
 
 exports.getRecommended = CatchAsync(async (req, res, next) => {
   const data = await Movie.recommendedMovies(req.params.id)
 
-  res.status(200).json({
-    status: 'sucess',
-    data,
-  })
+  if (data) {
+    res.status(200).json({
+      status: 'sucess',
+      data,
+    })
+  } else
+    res.status(404).json({
+      status: 'failed',
+    })
 })
 
 exports.getTop = CatchAsync(async (req, res, next) => {
   const data = await Movie.topMovies()
-  res.status(200).json({
-    status: 'sucess',
-    data,
-  })
+
+  if (data) {
+    res.status(200).json({
+      status: 'sucess',
+      data,
+    })
+  } else
+    res.status(404).json({
+      status: 'failed',
+    })
 })
 
 exports.searchMovies = CatchAsync(async (req, res, next) => {
   const data = await Movie.searchMovie(req.body.movieName)
 
-  res.status(200).json({
-    status: 'success',
-    data,
-  })
+  if (data) {
+    res.status(200).json({
+      status: 'success',
+      data,
+    })
+  } else
+    res.status(404).json({
+      status: 'failed',
+    })
 })
 
 exports.getPopularMovies = CatchAsync(async (req, res, next) => {
   const data = await Movie.popularMovies()
 
-  res.status(200).json({
-    status: 'success',
-    data,
-  })
+  if (data) {
+    res.status(200).json({
+      status: 'success',
+      data,
+    })
+  } else
+    res.status(404).json({
+      status: 'failed',
+    })
 })
 
 exports.getUpcomingMovies = CatchAsync(async (req, res, next) => {
   const data = await Movie.upcomingMovies()
 
-  res.status(200).json({
-    status: 'success',
-    data,
-  })
+  if (data) {
+    res.status(200).json({
+      status: 'success',
+      data,
+    })
+  } else
+    res.status(404).json({
+      status: 'failed',
+    })
 })
 
 exports.getMovieCast = CatchAsync(async (req, res, next) => {
   const data = await Movie.movieCast(req.params.id)
 
-  res.status(200).json({
-    status: 'success',
-    data,
-  })
+  if (data) {
+    res.status(200).json({
+      status: 'success',
+      data,
+    })
+  } else
+    res.status(404).json({
+      status: 'failed',
+    })
 })
 
 exports.getReviews = CatchAsync(async (req, res, next) => {
   const data = await Movie.movieReviews(req.params.id)
 
-  res.status(200).json({
-    status: 'success',
-    data,
-  })
+  if (data) {
+    res.status(200).json({
+      status: 'success',
+      data,
+    })
+  } else
+    res.status(404).json({
+      status: 'failed',
+    })
 })
